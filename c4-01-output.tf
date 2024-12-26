29d46b3b8fc6761590faefaf240bfed52e0314fa
Hardwin <trantrunghaupro2013@gmail.com>
2024-12-26 10:22:15 +0700

    edit output

--- c4-01-output.tf
+++ c4-01-output.tf
@@ -1,9 +1,9 @@
 output "AD_username" {
   description = "AWS Managed AD admin username"
-  value       = try(aws_directory_service_directory.ad.name, "")
+  value       = try(aws_directory_service_directory.ad[0].name, "")
 }
 
 output "AD_password" {
   description = "AWS Managed AD admin password"
-  value       = try(aws_directory_service_directory.ad.password, "")
+  value       = try(aws_directory_service_directory.ad[0].password, "")
 }
