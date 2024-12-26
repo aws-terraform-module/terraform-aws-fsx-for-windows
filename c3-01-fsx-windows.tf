bf52e9b3099bd168aa67b8885e4c92ceba81a33a
Hardwin <trantrunghaupro2013@gmail.com>
2024-12-26 11:35:27 +0700

    refactor module

--- c3-01-fsx-windows.tf
+++ c3-01-fsx-windows.tf
@@ -14,7 +14,7 @@ locals {
   ad_admin_password_create = length(var.ad_admin_password) == 0
   ad_admin_password        = local.ad_admin_password_create ? random_password.ad_admin_password[0].result : try(var.ad_admin_password[0], "")
 
-  active_directory_create = var.active_directory_id == "" ? 1 : 0
+  active_directory_create = var.active_directory_id == "" ? true : false
   active_directory_id     = local.active_directory_create ? aws_directory_service_directory.ad[0].id : var.active_directory_id
 
   ##########################################
@@ -35,11 +35,12 @@ locals {
   distinct_subnet_ids = var.subnet_ids != [] ? [
     for subnet_id in var.subnet_ids :
     subnet_id if !contains(
-      [for prev_subnet_id in slice(var.subnet_ids, 0, index(var.subnet_ids, subnet_id)) :
-      local.subnets_with_az[prev_subnet_id]],
+      [for prev_subnet_id in var.subnet_ids :
+      local.subnets_with_az[prev_subnet_id] if prev_subnet_id != subnet_id],
       local.subnets_with_az[subnet_id]
     )
   ] : []
+
 
   ##########################################
   # Step 3: Final Extracted Subnets (Ensure 2 distinct AZs)
@@ -87,7 +88,7 @@ resource "aws_fsx_windows_file_system" "fsx_windows" {
   storage_type                    = var.storage_type
   storage_capacity                = var.storage_capacity
   subnet_ids                      = var.subnet_ids == [] ? data.aws_subnets.selected.ids : var.subnet_ids
-  preferred_subnet_id             = var.preferred_subnet_id == "" ? local.extracted_subnets[0] : var.preferred_subnet_id
+  preferred_subnet_id             = var.preferred_subnet_id == "" ? (length(local.extracted_subnets) > 0 ? local.extracted_subnets[0] : null) : var.preferred_subnet_id
   deployment_type                 = var.deployment_type
   throughput_capacity             = var.throughput_capacity
   automatic_backup_retention_days = var.automatic_backup_retention_days
@@ -96,9 +97,9 @@ resource "aws_fsx_windows_file_system" "fsx_windows" {
     for_each = length(var.audit_log_configuration) > 0 ? [var.audit_log_configuration] : []
 
     content {
-      audit_log_destination             = try(audit_log_configuration.value.audit_log_destination, null)
-      file_access_audit_log_level       = try(audit_log_configuration.value.file_access_audit_log_level, null)
-      file_share_access_audit_log_level = try(audit_log_configuration.value.file_share_access_audit_log_level, null)
+      audit_log_destination             = try(each.value.audit_log_destination, null)
+      file_access_audit_log_level       = try(each.value.file_access_audit_log_level, null)
+      file_share_access_audit_log_level = try(each.value.file_share_access_audit_log_level, null)
     }
   }
 
@@ -106,10 +107,11 @@ resource "aws_fsx_windows_file_system" "fsx_windows" {
     for_each = length(var.disk_iops_configuration) > 0 ? [var.disk_iops_configuration] : []
 
     content {
-      iops = try(disk_iops_configuration.value.iops, null)
-      mode = try(disk_iops_configuration.value.mode, null)
+      iops = try(each.value.iops, null)
+      mode = try(each.value.mode, null)
     }
   }
+
 
   tags = merge(
     var.tags,
